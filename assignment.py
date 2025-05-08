db53362548f8a74069f43992779dbab6b9cce5e8
rinku-1992 <rinkupyautotest@gmail.com>
2025-05-08 14:00:08 +0530

    2nd change

--- assignment.py
+++ assignment.py
@@ -1,1 +1,32 @@
-print("hello")
+a=3
+b=8
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+
+
+a=5
+b=5.0
+print(a==b)
+print(a>=b)
+print(a!=b)
+
+a=5
+b=2
+print(a>b)
+print(a<b)
+
+a=5
+b=4
+print(a<=4)
+
+
+a=10
+c=-8
+d=[1,5,10,101]
+print(a in d)
+print(c in d)
